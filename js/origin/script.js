355fde0653ff894f3d543c9e92a9176426130bc2
racpast <yaojun.mail@qq.com>
2025-04-16 17:07:12 +0800

    更换图片为WebP

--- js/origin/script.js
+++ js/origin/script.js
@@ -51,6 +51,9 @@
       fixedHeight: $('[data-fixed-height]'),
       slick: $('.slick-slider'),
       d3Charts: $('.d3-chart'),
+      lightGallery: $("[data-lightgallery='group']"),
+      lightGalleryItem: $("[data-lightgallery='item']"),
+      lightDynamicGalleryItem: $("[data-lightgallery='dynamic']"),
       mailchimp: $('.mailchimp-mailform'),
       campaignMonitor: $('.campaign-mailform'),
       maps: $(".google-map-container"),
@@ -166,6 +169,100 @@
    */
   $(function () {
     var isNoviBuilder = window.xMode;
+
+
+    /**
+     * @desc Initialize the gallery with set of images
+     * @param {object} itemsToInit - jQuery object
+     * @param {string} addClass - additional gallery class
+     */
+    function initLightGallery(itemsToInit, addClass) {
+      if (!isNoviBuilder) {
+        $(itemsToInit).lightGallery({
+          thumbnail: $(itemsToInit).attr("data-lg-thumbnail") !== "false",
+          selector: "[data-lightgallery='item']",
+          autoplay: $(itemsToInit).attr("data-lg-autoplay") === "true",
+          pause: parseInt($(itemsToInit).attr("data-lg-autoplay-delay")) || 5000,
+          addClass: addClass,
+          mode: $(itemsToInit).attr("data-lg-animation") || "lg-slide",
+          loop: $(itemsToInit).attr("data-lg-loop") !== "false"
+        });
+      }
+    }
+
+    /**
+     * @desc Initialize the gallery with dynamic addition of images
+     * @param {object} itemsToInit - jQuery object
+     * @param {string} addClass - additional gallery class
+     */
+    function initDynamicLightGallery(itemsToInit, addClass) {
+      if (!isNoviBuilder) {
+        $(itemsToInit).on("click", function () {
+          $(itemsToInit).lightGallery({
+            thumbnail: $(itemsToInit).attr("data-lg-thumbnail") !== "false",
+            selector: "[data-lightgallery='item']",
+            autoplay: $(itemsToInit).attr("data-lg-autoplay") === "true",
+            pause: parseInt($(itemsToInit).attr("data-lg-autoplay-delay")) || 5000,
+            addClass: addClass,
+            mode: $(itemsToInit).attr("data-lg-animation") || "lg-slide",
+            loop: $(itemsToInit).attr("data-lg-loop") !== "false",
+            dynamic: true,
+            dynamicEl: JSON.parse($(itemsToInit).attr("data-lg-dynamic-elements")) || []
+          });
+        });
+      }
+    }
+
+    /**
+     * @desc Initialize the gallery with one image
+     * @param {object} itemToInit - jQuery object
+     * @param {string} addClass - additional gallery class
+     */
+    function initLightGalleryItem(itemToInit, addClass) {
+      if (!isNoviBuilder) {
+        $(itemToInit).lightGallery({
+          selector: "this",
+          addClass: addClass,
+          counter: false,
+          youtubePlayerParams: {
+            modestbranding: 1,
+            showinfo: 0,
+            rel: 0,
+            controls: 0
+          },
+          vimeoPlayerParams: {
+            byline: 0,
+            portrait: 0
+          }
+        });
+      }
+    }
+
+    /**
+     * Google map function for getting latitude and longitude
+     */
+    function getLatLngObject(str, marker, map, callback) {
+      var coordinates = {};
+      try {
+        coordinates = JSON.parse(str);
+        callback(new google.maps.LatLng(
+          coordinates.lat,
+          coordinates.lng
+        ), marker, map)
+      } catch (e) {
+        map.geocoder.geocode({'address': str}, function (results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+
+            callback(new google.maps.LatLng(
+              parseFloat(latitude),
+              parseFloat(longitude)
+            ), marker, map)
+          }
+        })
+      }
+    }
 
     /**
      * makeWaypointScroll
@@ -374,6 +471,10 @@
           });
         });
       }
+
+      c.on("initialized.owl.carousel", function () {
+        initLightGalleryItem(c.find('[data-lightgallery="item"]'), 'lightGallery-in-carousel');
+      });
 
       if (c.attr('data-nav-custom')) {
         c.on("initialized.owl.carousel", function (event) {
@@ -1291,6 +1392,41 @@
             }, 3500);
           }
         });
+      }
+    }
+
+
+    // lightGallery
+    if (plugins.lightGallery.length) {
+      for (var i = 0; i < plugins.lightGallery.length; i++) {
+        initLightGallery(plugins.lightGallery[i]);
+      }
+    }
+
+    // lightGallery item
+    if (plugins.lightGalleryItem.length) {
+      // Filter carousel items
+      var notCarouselItems = [];
+
+      for (var z = 0; z < plugins.lightGalleryItem.length; z++) {
+        if (!$(plugins.lightGalleryItem[z]).parents('.owl-carousel').length &&
+          !$(plugins.lightGalleryItem[z]).parents('.swiper-slider').length &&
+          !$(plugins.lightGalleryItem[z]).parents('.slick-slider').length) {
+          notCarouselItems.push(plugins.lightGalleryItem[z]);
+        }
+      }
+
+      plugins.lightGalleryItem = notCarouselItems;
+
+      for (var i = 0; i < plugins.lightGalleryItem.length; i++) {
+        initLightGalleryItem(plugins.lightGalleryItem[i]);
+      }
+    }
+
+    // Dynamic lightGallery
+    if (plugins.lightDynamicGalleryItem.length) {
+      for (var i = 0; i < plugins.lightDynamicGalleryItem.length; i++) {
+        initDynamicLightGallery(plugins.lightDynamicGalleryItem[i]);
       }
     }
 
